171be36d217bcb5572e4ec1511fc81fd18295062
Tobin Waters <tkwaterss@gmail.com>
2023-04-20 09:26:17 -0600

    error with id occuring

--- src/screens/EmployeeScreen.jsx
+++ src/screens/EmployeeScreen.jsx
@@ -1,25 +1,54 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
+import React, { useState, useRef } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
 const EmployeeScreen = () => {
-  const {id} = useParams()
-  const employees = useSelector((state) => state.employees)
+  const [showNoteArea, setShowNoteArea] = useState(false);
+  const { id } = useParams();
+  const employees = useSelector((state) => state.employees);
   const dispatch = useDispatch();
-  const {firstname, lastname, email, phone, position, salary, rating, notes, hireDate} = employees[id]
+  const {
+    firstname,
+    lastname,
+    email,
+    phone,
+    position,
+    salary,
+    rating,
+    notes,
+    hireDate,
+  } = employees[id];
+  const noteRef = useRef();
 
+  const toggleShow = () => setShowNoteArea(!showNoteArea);
 
+  const submitNote = event => {
+    event.preventDefault();
+    const payload = {
+      id,
+      newNote: noteRef.current.value,
+    }
+
+    dispatch({type: 'ADD_NOTE', payload})
+  }
   return (
     <div>
-      <h1>{firstname} {lastname}</h1>
+      <h1>
+        {firstname} {lastname}
+      </h1>
       <h2>Hired on {hireDate.toDateString()}</h2>
       <h3>Position: {position}</h3>
       <h3>Phone: {phone}</h3>
       <h3>Email: {email}</h3>
       <h3 className="colored-text">Salary: {salary}</h3>
-
+      <button onClick={toggleShow}>{showNoteArea ? "Hide Box" : "Add Note"}</button>
+      {showNoteArea && 
+      <form onSubmiit={submitNote}>
+        <textarea ref={noteRef} rows={7} cols={40}></textarea>
+        <button>Save Note</button>
+      </form>}
     </div>
-  )
-}
+  );
+};
 
-export default EmployeeScreen
+export default EmployeeScreen;
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -7,12 +7,19 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_EMPLOYEE":
-      return {...state, employees: [...state.employees, action.payload]}
+      return { ...state, employees: [...state.employees, action.payload] };
+    case "ADD_NOTE":
+      let newEmployeeState = [...state.employees]
+      newEmployeeState[action.payload.id].notes = [
+        ...newEmployeeState[action.payload.id].notes,
+        action.payload.newNote
+      ];
+      return {...state, employees: newEmployeeState}
     default:
       return state;
   }
 };
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 export default store;
